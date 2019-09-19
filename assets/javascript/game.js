e1a8c8743e04ce55f9ac46697ac41056a0c934c2
ggeorge <gmgeorge1993@gmail.com>
2019-09-18 20:46:17 -0500

    choosen word dispalys. onkey displays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,28 @@
-document.addEventListener("keyup", fucntion() {
-    alert('you clicked a button');
+var wins = 0;
+var lettersGuessed = [];
+const totalNumGuesses = 12;
+var winningWords = ['Baseball' , 'football' , 'basektball' , 'soccer'];
+
+function refreshUI (){
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('guesses-left').innerHTML = totalNumGuesses - lettersGuessed.length;
+    document.getElementById('letters').innerHTML = lettersGuessed.join(',');
+}
+
+function chooseWinningWord () {
+    var randWord = winningWords[Math.floor(Math.random() * winningWords.length)];
+    console.log(randWord);
+};
+
+
+
+refreshUI();
+chooseWinningWord();
+
+
+document.addEventListener('keyup', function (event) {
+    lettersGuessed.push(event.keyCode);
+    
+    
+    refreshUI();
 });
