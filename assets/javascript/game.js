55ef07762d1e1887251ac010b87002eb4e31b382
ggeorge <gmgeorge1993@gmail.com>
2019-09-18 21:33:55 -0500

    added underscores and onkey to text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,12 @@ var wins = 0;
 var lettersGuessed = [];
 const totalNumGuesses = 12;
 var winningWords = ['Baseball' , 'football' , 'basektball' , 'soccer'];
+var underScore = [];
 
 function refreshUI (){
     document.getElementById('wins').innerHTML = wins;
     document.getElementById('guesses-left').innerHTML = totalNumGuesses - lettersGuessed.length;
-    document.getElementById('letters').innerHTML = lettersGuessed.join(',');
+    document.getElementById('letters').innerHTML = lettersGuessed.join('');
 }
 
 function chooseWinningWord () {
@@ -14,15 +15,23 @@ function chooseWinningWord () {
     console.log(randWord);
 };
 
+function generateUnderscore () {
+    for (var i = 0; i < winningWords.length; i++) {
+        underScore.push('_');
+    }
+    return underScore;
+}
 
-
+console.log(generateUnderscore());
 refreshUI();
 chooseWinningWord();
 
 
-document.addEventListener('keyup', function (event) {
-    lettersGuessed.push(event.keyCode);
-    
-    
+document.addEventListener('keypress', function (event) {
+    let keycode = event.keyCode;
+    let keyword = String.fromCharCode(keycode);
+    if (randWord.indexOf(keyword) > - 1){
+        console.log(true);
+    }    
     refreshUI();
 });
